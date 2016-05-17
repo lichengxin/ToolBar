6d23746ef55c721bb34cc103b7582ed093e6bc9c
li <lichengxin>
2016-05-17 18:05:39 +0800

    add

--- app/src/main/java/com/android/toolbar/fragment/TabFragment1.java
+++ app/src/main/java/com/android/toolbar/fragment/TabFragment1.java
@@ -7,16 +7,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.toolbar.R;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class TabFragment1 extends Fragment {
 
 
+    @Bind(R.id.mSlider)
+    SliderLayout mSlider;
+    @Bind(R.id.mCustom_indicator)
+    PagerIndicator mCustomIndicator;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
