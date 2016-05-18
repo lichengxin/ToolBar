37b011d561a5557ecd60232e664c2e9fc1b896fe
li <lichengxin>
2016-05-18 11:25:20 +0800

    add

---
+++ app/src/main/java/com/android/toolbar/contants/Urlcontants.java
@@ -0,0 +1,8 @@
+package com.android.toolbar.contants;
+
+/**
+ * Created by li on 2016/5/18.
+ */
+public class Urlcontants {
+
+}
--- app/src/main/java/com/android/toolbar/fragment/TabFragment1.java
+++ app/src/main/java/com/android/toolbar/fragment/TabFragment1.java
@@ -5,16 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.toolbar.R;
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class TabFragment1 extends Fragment {
+public class TabFragment1 extends Fragment implements BaseSliderView.OnSliderClickListener {
 
 
     @Bind(R.id.mSlider)
@@ -28,12 +33,43 @@ public class TabFragment1 extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
         ButterKnife.bind(this, view);
+        initBanner();
         return view;
+    }
+
+    private void initBanner() {
+        HashMap<String, Integer> file_maps = new HashMap<>();
+        file_maps.put("Hannibal", R.mipmap.hannibal);
+        file_maps.put("Big Bang Theory", R.mipmap.bigbang);
+        file_maps.put("House of Cards", R.mipmap.house);
+        file_maps.put("Game of Thrones", R.mipmap.game_of_thrones);
+        for (String name:file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            textSliderView.description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra", name);
+            mSlider.addSlider(textSliderView);
+
+        }
+        //设置滚动方式
+        mSlider.setPresetTransformer(SliderLayout.Transformer.Default);
+        //设置indicator位置
+        mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(getActivity(),slider.getBundle().getString("extra")+"===",Toast.LENGTH_LONG).show();
     }
 }
